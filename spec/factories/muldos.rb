081e9e7f97ffe45d5b0599710054b1c20c672f90
ylogik <ly.goki@gmail.com>
2019-09-23 17:06:46 +0200

    Make the factory color for muldo a valid one.

--- spec/factories/muldos.rb
+++ spec/factories/muldos.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :muldo do
     sequence(:name) { |n| "Dd#{n}" }
     association :owner
-    color 'Doree'
+    color 'Dore'
     reproduction 4
     sex 'M'
     pregnant false
