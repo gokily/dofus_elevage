e661b1f5e5c4420f939b566e1cd15101edc13b91
ylogik <ly.goki@gmail.com>
2019-12-04 12:09:14 +0100

    Add mailer host to production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dofus_elevage_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'https://cryptic-reef-00218.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
