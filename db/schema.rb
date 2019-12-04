be2dfad12c991bf6312de2d0fb8d5e99a80a1d7a
ylogik <ly.goki@gmail.com>
2019-12-04 11:49:07 +0100

    And again.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_14_100032) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "mounts", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
